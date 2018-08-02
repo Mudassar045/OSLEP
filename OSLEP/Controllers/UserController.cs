347117b09fb5007a7611accb24fe14d7bc2af45f
Mudassar Ali <Mudassaralics007@gmail.com>
2018-08-02 10:51:15 +0500

    OSLEP template completed

---
+++ OSLEP/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OSLEP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(String userName, String userPassword)
+        {
+            return View();
+        }
+    }
+}
